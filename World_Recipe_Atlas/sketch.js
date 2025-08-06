30eddc1bc82d8509c2394ad04fded8f1264779e9
LynnGan <115210189+LynnGan@users.noreply.github.com>
2025-08-06 23:10:51 +0800

    added recipe, loaded json file into sketch

--- World_Recipe_Atlas/sketch.js
+++ World_Recipe_Atlas/sketch.js
@@ -1,14 +1,32 @@
+let mySphere;
+let recipe;
+
+function preload() {
+  recipe = loadJSON("assets/recipe.json");
+}
+
+
+
 function setup() {
+  console.log(recipe[0].name);
   createCanvas(windowWidth, windowHeight, WEBGL);
+  mySphere = new referenceSphere();
 }
 
 function draw() {
   background(30);
-  orbitControl();
-  rotateY(frameCount * -0.002);
-  rotateX(frameCount * -0.002);
-  noFill();
-  stroke(255, 30);
-  strokeWeight(1);
-  sphere(windowWidth / 6, 10, 10);
+  mySphere.display();
+}
+
+
+class referenceSphere {
+  display() {
+    orbitControl();
+    rotateY(frameCount * -0.002);
+    rotateX(frameCount * -0.002);
+    noFill();
+    stroke(255, 30);
+    strokeWeight(1);
+    sphere(windowWidth / 6, 10, 10);
+  }
 }

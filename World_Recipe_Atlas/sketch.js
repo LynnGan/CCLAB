54877924ffcf095d1ef5c1e4a4c4fa59fe631c07
LynnGan <115210189+LynnGan@users.noreply.github.com>
2025-08-19 00:01:39 +0800

    Update sketch.js

--- World_Recipe_Atlas/sketch.js
+++ World_Recipe_Atlas/sketch.js
@@ -294,7 +294,7 @@ class Popup {
       textSize(16);
       textStyle(ITALIC);
       textSize(20);
-      text("✨ A midnight snack for dreamers ✨", this.x + this.w / 2, this.y - 10);
+      text("✨ A midnight snack for dreamers ✨", this.x + this.w / 2, this.y - 16);
       textStyle(NORMAL);
     }
     //box
